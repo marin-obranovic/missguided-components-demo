50523e3cff42190bd15e73d75a70d40f4ab7da56
Marin Obranovic <marin.obranovic@missguided.com>
2021-02-08 16:02:10 +0000

    change how building is done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 
-import { PageTitle } from "./components/PageTitle";
-import { TextBlock } from "./components/TextBlock";
-import { DiscountBox } from "./components/DiscountBox";
+import { PageTitle } from "./components/PageTitle/index.jsx";
+import { TextBlock } from "./components/TextBlock/index.jsx";
+import { DiscountBox } from "./components/DiscountBox/index.jsx";
 
 export { PageTitle, TextBlock, DiscountBox };
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,88 @@
+// // var glob = require("glob");
+// var path = require("path");
+// // const autoprefixer = require("autoprefixer");
+
+// const publicDir = path.resolve(__dirname, "dist");
+
+// module.exports = {
+//   target: "web",
+//   plugins: [
+//     new ExtractCssChunks({
+//       filename: "[name].css",
+//       cssModules: true,
+//     }),
+//     // new MiniCssExtractPlugin({
+//     //   // Options similar to the same options in webpackOptions.output
+//     //   // both options are optional
+//     //   filename: "[name].css",
+//     //   chunkFilename: "[id].css",
+//     // }),
+//   ],
+
+//   exclude: {
+//     test: [/\.(js|jsx)$/],
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.scss$/,
+//         use: [
+//           {
+//             loader: ExtractCssChunks.loader,
+//             options: {
+//               hmr: false,
+//             },
+//           },
+//           {
+//             loader: "sass-loader",
+//             options: {
+//               sourceMap: false,
+//             },
+//           },
+//           {
+//             loader: "postcss-loader",
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   output: {
+//     path: publicDir,
+//     filename: "[name].bundle.js",
+//     publicPath: "/dist/",
+//   },
+// };
+
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "umd",
+    library: "missguided-components",
+  },
+  target: "web",
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env", "@babel/preset-react"] },
+        },
+      },
+      {
+        test: /\.scss$/,
         use: [
-          "style-loader",
-          "css-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              implementation: require("sass"),
-              sassOptions: {
-                fiber: false,
-              },
-            },
-          },
+          { loader: "style-loader" },
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader" },
+          { loader: "postcss-loader" },
+          { loader: "sass-loader" },
         ],
       },
     ],
@@ -28,5 +94,8 @@ module.exports = {
       filename: "[name].css",
       chunkFilename: "[id].css",
     }),
+    new CleanWebpackPlugin(),
   ],
 };
+
+//MiniCssExtractPlugin.loader, "postcss-loader",
